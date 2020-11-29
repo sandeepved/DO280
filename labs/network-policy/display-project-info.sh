23817e1d1b3f949eda0052f5d43e260ffc1938fc
Student User <student@workstation.lab.example.com>
2020-11-29 02:54:00 -0500

    display-project-info.sh

--- labs/network-policy/display-project-info.sh
+++ labs/network-policy/display-project-info.sh
@@ -18,7 +18,9 @@ if oc get project -o jsonpath='{.items[*].metadata.name}' | grep -q network-test
 then
   echo "PROJECT: network-test"
   echo
-  oc get pods -o custom-columns="POD NAME:.metadata.name" -n network-test
+  oc get pods -o custom-columns="POD NAME:.metadata.name,IP ADDRESS:.status.podIP" -n network-test
+  echo
+  oc get svc -o custom-columns="SERVICE NAME:.metadata.name,CLUSTER-IP:.spec.clusterIP" -n network-policy
   echo
   echo "==================================================================="
 fi
